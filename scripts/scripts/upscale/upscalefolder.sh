1311661e029c3014323e2324ba1e0d58a6078ce1
Jason <jasonpenilla2@me.com>
2019-09-08 22:04:39 -0700

    clean lock script up

--- scripts/scripts/upscale/upscalefolder.sh
+++ scripts/scripts/upscale/upscalefolder.sh
@@ -10,6 +10,6 @@ cd ./i
 for i in *.png
     do waifu2x-converter-cpp --force-OpenCL -j 4 --scale-ratio 2 -m scale -i $i -o ../o/x2_$i
 done
-for i in *.jpg
-    do waifu2x-converter-cpp --force-OpenCL -j 4 --scale-ratio 2 -m scale -i $i -o ../o/x2_$i
+for k in *.jpg
+    do waifu2x-converter-cpp --force-OpenCL -j 4 --scale-ratio 2 -m scale -i $k -o ../o/x2_$k
 done
